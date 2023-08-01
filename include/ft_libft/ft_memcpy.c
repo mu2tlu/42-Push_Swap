b72bb3dd88cbf465ab366191a60997362011edc2
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-01 21:52:55 +0300

    b

---
+++ include/ft_libft/ft_memcpy.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/31 17:44:09 by mumutlu           #+#    #+#             */
+/*   Updated: 2023/07/31 17:44:10 by mumutlu          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*d;
+	unsigned char	*s;
+
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (d == s)
+		return (dst);
+	while (n--)
+		*d++ = *s++;
+	return (dst);
+}
