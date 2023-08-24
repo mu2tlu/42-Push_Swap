bd357d708301266f4e4442e90457c5cef927d04a
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-24 21:10:26 +0300

    42

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:31:56 by mumutlu           #+#    #+#             */
-/*   Updated: 2023/08/21 19:44:27 by mumutlu          ###   ########.fr       */
+/*   Updated: 2023/08/24 20:56:57 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,12 +20,69 @@ void	rrr(t_stack **stack_a, t_stack **stack_b)
 	write(1, "rrr\n", 4);
 }
 
+int	data_check(t_stack *x, t_stack *y)
+{
+	return (x->data > y->data);
+}
+
+int	data_short(t_stack **stack)
+{
+	if (!(*stack)->next)
+		return (0);
+	while ((*stack)->next)
+	{
+		if (data_check(stack, (*stack)->next))
+			(*stack) = (*stack)->next;
+		else
+			return (0);
+	}
+	return (1);
+}
+
 void	the_rules(t_stack **stack_a, t_stack **stack_b)
 {
-	(void)stack_b;
-	//sa(stack_a, 1);
-	pb(stack_a, stack_b);
-	printnode(*stack_a);
-	write(1, "\n", 1);
+	while (*stack_a)
+	{
+		if ((*stack_a)->next)
+		{
+			break ;
+		}
+		else
+		{
+			if ((*stack_b))
+			{
+				while ((*stack_b))
+				{
+					if ((*stack_b)->next)
+					{
+						pb(stack_a, stack_b);
+						if (data_check(*stack_b, (*stack_b)->next))
+						{
+							rb(stack_b, 1);
+						}
+						else
+						{
+							
+						}
+						
+					}
+					else
+					{
+						pa(stack_a, stack_b);
+						if (data_check(*stack_a, (*stack_a)->next))
+						{
+							sa(stack_a, 1);
+						}
+						else
+							break ;
+					}
+				}
+			}
+			else
+				break ;
+		}
+	}
+
 	printnode(*stack_b);
+	printnode(*stack_a);
 }
