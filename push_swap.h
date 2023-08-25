33fb6fa70952f4ae2f4ada89f9a9913bbb5858f3
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-25 15:14:35 +0300

    42

--- push_swap.h
+++ push_swap.h
@@ -47,9 +47,9 @@ void	rr(t_stack **stack_a, t_stack **stack_b);
 void	rra(t_stack **stack_a, int a);
 void	rrb(t_stack **stack_b, int b);
 void	rrr(t_stack **stack_a, t_stack **stack_b);
-int		data_check(t_stack *x, t_stack *y);
-int		data_short(t_stack *stack);
-int		stack_len(t_stack *stack);
+int		d_check(t_stack *x, t_stack *y);
+int		d_short(t_stack *stack);
+int		s_len(t_stack *stack);
 
 void	printnode(t_stack *node);//dikakt
 #endif
--- rules.c
+++ rules.c
@@ -6,21 +6,21 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:31:56 by mumutlu           #+#    #+#             */
-/*   Updated: 2023/08/25 15:02:17 by mumutlu          ###   ########.fr       */
+/*   Updated: 2023/08/25 15:13:49 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <unistd.h>
 
-int	data_check(t_stack *x, t_stack *y)
+int	d_check(t_stack *x, t_stack *y)
 {
 	if (!x && !y)
 		return (0);
 	return (x->data > y->data);
 }
 
-int	stack_len(t_stack *stack)
+int	s_len(t_stack *stack)
 {
 	int	i;
 
@@ -35,13 +35,13 @@ int	stack_len(t_stack *stack)
 	return (i);
 }
 
-int	data_short(t_stack *stack)
+int	d_short(t_stack *stack)
 {
 	if (!stack->next)
 		return (0);
 	while (stack->next)
 	{
-		if (data_check(stack, stack->next))
+		if (d_check(stack, stack->next))
 			stack = stack->next;
 		else
 			return (0);
@@ -55,9 +55,12 @@ void	the_rules(t_stack **stack_a, t_stack **stack_b)
 
 	i = stack_len(*stack_a);
 	if (i == 2)
-		if (data_check(*stack_a, (*stack_a)->next))
+		if (d_check(*stack_a, (*stack_a)->next))
 			sa(stack_a, 1);
-	data_short(*stack_a);
+	else if (i == 3)
+	{
+		
+	}
 	printnode(*stack_b);
 	printnode(*stack_a);
 }
