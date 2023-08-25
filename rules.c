1e1e95a2d897206df07bf83b1dbabff65fe0dcb8
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-25 14:01:25 +0300

    42

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:31:56 by mumutlu           #+#    #+#             */
-/*   Updated: 2023/08/24 20:56:57 by mumutlu          ###   ########.fr       */
+/*   Updated: 2023/08/25 14:00:52 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,11 @@ int	data_check(t_stack *x, t_stack *y)
 
 int	data_short(t_stack **stack)
 {
-	if (!(*stack)->next)
+	if (!(*stack) || (*stack)->next)
 		return (0);
 	while ((*stack)->next)
 	{
-		if (data_check(stack, (*stack)->next))
+		if (data_check(*stack, (*stack)->next))
 			(*stack) = (*stack)->next;
 		else
 			return (0);
@@ -56,23 +56,26 @@ void	the_rules(t_stack **stack_a, t_stack **stack_b)
 					if ((*stack_b)->next)
 					{
 						pb(stack_a, stack_b);
-						if (data_check(*stack_b, (*stack_b)->next))
-						{
-							rb(stack_b, 1);
-						}
-						else
-						{
-							
-						}
-						
+							if (data_check(*stack_b, (*stack_b)->next))
+							{
+								rb(stack_b, 1);
+								rb(stack_b, 1);
+								sb(stack_b, 1);
+								if(data_short(stack_b))
+									exit(1);
+							}
+							else
+							{
+								rrb(stack_b, 1);
+								if(data_short(stack_b))
+									exit(1);
+							}
 					}
 					else
 					{
 						pa(stack_a, stack_b);
 						if (data_check(*stack_a, (*stack_a)->next))
-						{
 							sa(stack_a, 1);
-						}
 						else
 							break ;
 					}
@@ -81,6 +84,7 @@ void	the_rules(t_stack **stack_a, t_stack **stack_b)
 			else
 				break ;
 		}
+		data_short(stack_a);
 	}
 
 	printnode(*stack_b);
