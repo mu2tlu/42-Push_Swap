2233ec3ca1e11b133737b425ff5e39ea35bc2ef6
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-25 19:20:15 +0300

    42

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:31:56 by mumutlu           #+#    #+#             */
-/*   Updated: 2023/08/25 15:51:43 by mumutlu          ###   ########.fr       */
+/*   Updated: 2023/08/25 19:14:47 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,30 +49,41 @@ int	d_ascending(t_stack *stack)
 	return (1);
 }
 
+void	argv_3(t_stack **stack_a)
+{
+	if (d_check(*stack_a, (*stack_a)->next)
+		&& d_check(*stack_a, (*stack_a)->next->next))
+	{
+		ra(stack_a, 1);
+		if (d_check(*stack_a, (*stack_a)->next))
+			sa(stack_a, 1);
+	}
+	else if (!d_check(*stack_a, (*stack_a)->next) 
+		&& !d_check(*stack_a, (*stack_a)->next->next))
+	{
+		rra(stack_a, 1);
+		if (d_check(*stack_a, (*stack_a)->next))
+			sa(stack_a, 1);
+	}
+	else if (!d_check(*stack_a, (*stack_a)->next) 
+		&& d_check(*stack_a, (*stack_a)->next->next))
+	{
+		rra(stack_a, 1);
+	}
+	else if (d_check(*stack_a, (*stack_a)->next)
+		&& !d_check(*stack_a, (*stack_a)->next->next))
+		sa(stack_a, 1);
+}
+
 void	the_rules(t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
 
 	i = s_len(*stack_a);
-	if (i == 2)
-	{
-		if (d_check(*stack_a, (*stack_a)->next))
-			sa(stack_a, 1);
-	}
+	if (i == 2 && d_check(*stack_a, (*stack_a)->next))
+		sa(stack_a, 1);
 	else if (i == 3)
-	{
-		if (!d_ascending(*stack_a))
-		{
-			if (d_check(*stack_a, (*stack_a)->next))
-			{
-				write(1, "a", 1);
-			}
-			else
-			{
-				write(1, "1", 1);
-			}
-		}
-	}
+		argv_3(stack_a);
 	printnode(*stack_a);
 	printnode(*stack_b);
 }
