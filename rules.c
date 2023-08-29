6fa3bb19446e0a18e2722e4d078a16c702d5e62b
Muhammet Mutlu <115688354+mu2tlu@users.noreply.github.com>
2023-08-29 21:39:07 +0300

    42

--- rules.c
+++ rules.c
@@ -6,7 +6,7 @@
 /*   By: mumutlu <mumutlu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/18 17:31:56 by mumutlu           #+#    #+#             */
-/*   Updated: 2023/08/29 20:46:26 by mumutlu          ###   ########.fr       */
+/*   Updated: 2023/08/29 21:38:27 by mumutlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,15 +38,6 @@ void	argv_2_3(t_stack **s, int i)
 	}
 }
 
-void	argv_stack(t_stack **stack_a, t_stack **stack_b, int i)
-{
-	while (*stack_a)
-	{
-		if (d_ascending(*stack_a) && i == s_len(*stack_a))
-			break ;
-	}
-}
-
 void	the_rules(t_stack **stack_a, t_stack **stack_b)
 {
 	int	i;
@@ -54,8 +45,6 @@ void	the_rules(t_stack **stack_a, t_stack **stack_b)
 	i = s_len(*stack_a);
 	if (i < 4)
 		argv_2_3(stack_a, i);
-	else
-		argv_stack(stack_a, stack_b, i);
 	printnode(*stack_a);
 	printnode(*stack_b);
 }
